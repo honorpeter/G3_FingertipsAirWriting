6e9dc5bbc662ee3be8c42ed5c28d56bbdfe28e73
lingujiang <32206850+lingujiang@users.noreply.github.com>
2018-02-05 20:08:30 -0500

    add google search function

--- server_py/server_display.py
+++ server_py/server_display.py
@@ -14,6 +14,7 @@ to the ASCII characters 'POST' followed immediately by four
 bytes that contain the new value to set to SERVER_VALUE.
 """
 import socket
+import webbrowser
 
 PORT = 9090
 BUFFER_SIZE = 20
@@ -25,26 +26,28 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     # Bind to port on any interface
     sock.bind(('192.168.1.102', PORT))
-    sock.listen(1) # allow backlog of 1
+    sock.listen(1)  # allow backlog of 1
 
     print("BEGIN LISTENING ON PORT", PORT)
     # Begin listening for connections
-    while(True):
-       conn, addr = sock.accept()
-       with conn:
-           print("\nCONNECTION:", addr)
+    while (True):
+        conn, addr = sock.accept()
+        with conn:
+            print("\nCONNECTION:", addr)
 
-           # Receive and handle command
-           data = conn.recv(BUFFER_SIZE)
-           if data[:4] == b'POST':
-               SERVER_VALUE = data[4:]
-               string_in = str(bytes.fromhex(SERVER_VALUE.hex()))
-               string_in = string_in[2:18]
-               print("POST VALUE:", string_in)
-           elif data == b'GET':
-              conn.send(SERVER_VALUE)
-              print("GET", SERVER_VALUE.hex())
-           else:
-               print("BAD COMMAND RECEIVED")
-           conn.shutdown(socket.SHUT_RDWR)
-           conn.close()
+            # Receive and handle command
+            data = conn.recv(BUFFER_SIZE)
+            if data[:4] == b'POST':
+                SERVER_VALUE = data[4:]
+                string_in = str(bytes.fromhex(SERVER_VALUE.hex()))
+                string_in = string_in[2:18]
+                print("POST VALUE:", string_in)
+                google_url = 'https://www.google.com/search?q={}&num={}&hl={}'.format("cat", 100, "en")
+                webbrowser.open_new(google_url)
+            elif data == b'GET':
+                conn.send(SERVER_VALUE)
+                print("GET", SERVER_VALUE.hex())
+            else:
+                print("BAD COMMAND RECEIVED")
+            conn.shutdown(socket.SHUT_RDWR)
+            conn.close()
